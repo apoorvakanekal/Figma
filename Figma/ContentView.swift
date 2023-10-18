03428abc615494f57cd88364be3fe9df258a409c
Apoorva Kanekal <apka8010@colorado.edu>
2023-10-18 12:32:12 -0600

    Initial Commit

---
+++ Figma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Figma
+//
+//  Created by Apoorva Kanekal on 10/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Figma/FigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaApp.swift
+//  Figma
+//
+//  Created by Apoorva Kanekal on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
